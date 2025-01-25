c5065930b3651e8aba6f200f319ae4d9ee99d68d
DENN319 <davuth.den@gmail.com.com>
2025-01-25 11:42:59 +0700

    update

---
+++ backtest.sh
@@ -0,0 +1,1 @@
+docker compose run --rm freqtrade backtesting --strategy-path user_data/strategies -c user_data/config.json --breakdown day --export none --timerange 20250101-
---
+++ download-binance-spot.sh
@@ -0,0 +1,1 @@
+docker compose run --rm freqtrade download-data -c user_data/config.json --exchange binance --days 365 -t 5m 15m 1h 4h 1d --trading-mode spot 
