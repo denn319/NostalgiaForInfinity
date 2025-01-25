a7d3c92866e7848cbdae9281692109657d5e2be1
DENN319 <davuth.den@gmail.com.com>
2025-01-25 13:54:29 +0700

    Hyperopt

--- hyperopt.sh
+++ hyperopt.sh
@@ -1,1 +1,1 @@
-docker compose run --rm freqtrade hyperopt --hyperopt-loss OnlyProfitHyperOptLoss --space roi --strategy-path user_data/strategies -c user_data/config.json -e 50 -j -10
+docker compose run --rm freqtrade hyperopt --hyperopt-loss OnlyProfitHyperOptLoss --space roi --strategy-path user_data/strategies -c user_data/config.json --timerange 20240701- -e 100
