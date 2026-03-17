ce1610bc7313be3010b55cde0e46665687f61764
iterativ <vangelis.dimou@tutanota.com>
2026-03-17 15:51:43 +0200

    X7: signal 4: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -13694,6 +13694,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["RSI_3_1h"] > 5.0) | (df["ROC_9_1d"] < 100.0))
             # 1h & 4h down move, 1h still high
             & ((df["RSI_3_1h"] > 10.0) | (df["RSI_3_4h"] > 20.0) | (df["AROONU_14_1h"] < 50.0))
+            # 1h & 4h down move, 1h downtrend
+            & ((df["RSI_3_1h"] > 10.0) | (df["RSI_3_4h"] > 15.0) | (df["ROC_9_1h"] > -20.0))
             # 1h & 1d down move, 1d high
             & ((df["RSI_3_1h"] > 10.0) | (df["RSI_3_1d"] > 45.0) | (df["AROONU_14_1d"] < 70.0))
             # 1h down move, 4h high
