a4d6b3264b17c86d6d72eeff3ba5eedac281b19d
iterativ <vangelis.dimou@tutanota.com>
2026-03-20 09:12:08 +0200

    X7: signal 41: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -15188,8 +15188,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["RSI_3_15m"] > 10.0) | (df["RSI_3_1d"] > 25.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 80.0))
             # 15m down move, 15m still high, 1d overbought
             & ((df["RSI_3_15m"] > 10.0) | (df["AROONU_14_15m"] < 50.0) | (df["ROC_9_1d"] < 30.0))
-            # 15m down move, 15m & 1h high
-            & ((df["RSI_3_15m"] > 10.0) | (df["AROONU_14_15m"] < 70.0) | (df["AROONU_14_1h"] < 90.0))
+            # 15m down move, 15m high
+            & ((df["RSI_3_15m"] > 10.0) | (df["AROONU_14_15m"] < 70.0))
             # 15m down move, 1h high, 15m downtrend
             & ((df["RSI_3_15m"] > 10.0) | (df["AROONU_14_1h"] < 80.0) | (df["ROC_9_15m"] > -10.0))
             # 15m down move, 1h high, 1d downtrend
@@ -15296,6 +15296,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["RSI_3_1h"] > 3.0) | (df["AROONU_14_4h"] < 70.0))
             # 1h down move, 1d high
             & ((df["RSI_3_1h"] > 3.0) | (df["AROONU_14_1d"] < 70.0))
+            # 1h down move, 4h still high, 1d downtrend
+            & ((df["RSI_3_1h"] > 3.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 50.0) | (df["ROC_9_1h"] > -20.0))
             # 1h down move, 1d high & overbought
             & ((df["RSI_3_1h"] > 3.0) | (df["STOCHRSIk_14_14_3_3_1d"] < 60.0) | (df["ROC_9_1d"] < 20.0))
             # 1h & 4h & 1d down move
