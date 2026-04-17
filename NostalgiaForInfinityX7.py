04499581509e133e37b33f20ad98dcf9d64d5867
iterativ <vangelis.dimou@tutanota.com>
2026-04-17 16:01:15 +0300

    X7: signal 501: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -69,7 +69,7 @@ class NostalgiaForInfinityX7(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v17.3.1115"
+    return "v17.3.1116"
 
   stoploss = -0.99
 
@@ -23274,6 +23274,8 @@ class NostalgiaForInfinityX7(IStrategy):
           short_entry_logic.append((df["RSI_3_4h"] < 90.0) | (df["AROONU_14_1h"] > 40.0))
           # 4h up move, 1d still low, 4h uptrend
           short_entry_logic.append((df["RSI_3_4h"] < 85.0) | (df["RSI_14_1d"] > 40.0) | (df["ROC_9_4h"] < 20.0))
+          # 4h up move, 4h still low
+          short_entry_logic.append((df["RSI_3_4h"] < 80.0) | (df["STOCHRSIk_14_14_3_3_4h"] > 50.0))
           # 4h up move, 1h low
           short_entry_logic.append((df["RSI_3_4h"] < 70.0) | (df["AROONU_14_1h"] > 25.0))
           # 4h up move, 1d low
